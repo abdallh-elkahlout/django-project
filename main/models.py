89049354d6add4cd0a34518dd0ba2e38516a2246
abdallh <abd>
2023-02-13 05:24:02 +0200

    create model question

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# ORM Code
+
+
+class Question(models.Model): # انشاء جدول وعملتله وراثة من كلاس موديلز والجدول عبارة عن  تخزين اسئلة على الداتا بيز 
+    # انشاء الحقول
+    question_text = models.CharField(max_length=12) # كاركتير واقصى طول للحقل هو 12
+    created_at = models.DateTimeField(auto_now_add=True) # متى أنشأت السؤال يعني ايش التاريه اللي يسجله لتخزين السؤال في الجدول
+
+
+    def __str__(self):
+        # هذه عشان لما يعرض الحقل المخزن في الموقع في صفحة الادمن يعرض النص اللي دخله المستخدم مش اوبجكت 1 وابويجكت 2 وهكذا لا بدي اياه يعرضالنص المدحل
+        return self.question_text
